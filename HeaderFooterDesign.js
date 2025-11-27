e3bd550a64ec10359e9b78d47184e8d2bd2be7e2
kiwi <romeofrancesco@yahoo.com>
2025-11-27 17:25:18 +0100

    header correct for both desktop and mobile version

--- HeaderFooterDesign.js
+++ HeaderFooterDesign.js
@@ -1,19 +1,34 @@
-class SpecialHeader extends HTMLElement{
+class SpecialHeaderDesktop extends HTMLElement{
     connectedCallback () {
         this.innerHTML = `
-        <header>
+        <header class="desktop-header">
             <nav>
-                <a href="index.html" class="active"><i class="fa-solid fa-house-chimney fa-lg"></i> Home</a>
-                <a href="research.html"><i class="fa-solid fa-microscope fa-lg"></i> Research</a>
-                <a href="publications.html"><i class="fa-solid fa-newspaper fa-lg"></i> Publications</a>
+                <a href="index.html" class="active"><i class="fa-solid fa-house-chimney fa-lg"></i>Home</a>
+                <a href="research.html"><i class="fa-solid fa-microscope fa-lg"></i>Research</a>
+                <a href="publications.html"><i class="fa-solid fa-newspaper fa-lg"></i>Publications</a>
             </nav>
         <header>
         `
     }
 }
 
+customElements.define('special-header-desktop', SpecialHeaderDesktop)
 
-customElements.define('special-header', SpecialHeader)
+class SpecialHeaderMobile extends HTMLElement{
+    connectedCallback () {
+        this.innerHTML = `
+        <header class="mobile-header">
+            <nav>
+                <a href="index.html" title="Home"><i class="fa-solid fa-house-chimney fa-lg"></i></a>
+                <a href="research.html" title="Research"><i class="fa-solid fa-microscope fa-lg"></i></a>
+                <a href="publications.html" title="Publications"><i class="fa-solid fa-newspaper fa-lg"></i></a>
+            </nav>
+        <header>
+        `
+    }
+}
+
+customElements.define('special-header-mobile', SpecialHeaderMobile)
 
 
 
